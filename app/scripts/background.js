8e3c161963b35e6246802b79d8262a91b3603dfd
phette23 <phette23@gmail.com>
2014-07-22 21:59:13 -0700

    initial commit

---
+++ app/scripts/background.js
@@ -0,0 +1,119 @@
+/* global $ */
+/*
+ * background.js:
+ * listen for messages from contentscript.js
+ * messages contain wp object with info about article
+ * construct DPLA API queries using that info
+ * when we have some API results, sendResponse back to contentscript.js
+ */
+
+'use strict';
+
+// construct a query URI
+var query = '',
+buildURI = function (query) {
+    var base = 'http://api.dp.la/v2/items',
+        key = 'e4c036f3302aad8d8c188683967b9619';
+
+    return base + '?api_key=' + key + '&q=' + encodeURIComponent(query);
+},
+// truncate string if too long & add …
+trunc = function (str, int) {
+    // default to 60 char cutoff
+    var cutoff = parseInt(int, 10) || 60,
+        // lots of Hathi Trust titles end in ' /'
+        newStr = str.replace(/(\s\/)$/, '');
+
+    if (newStr.length > cutoff) {
+        // trim trailing whitespace of substring
+        return newStr.substr(0, cutoff).replace(/\s$/,'') + '&hellip;';
+    } else {
+        return newStr;
+    }
+},
+// given DPLA doc, see if its type array contains 'image'
+isItAnImage = function (types) {
+    // types can be array or string
+    if ($.isArray(types)) {
+        for (var type in types) {
+            if (type.toLowerCase() === 'image') {
+                return true;
+            }
+        }
+        return false;
+    } else if (types && types.toLowerCase() === 'image') {
+        return true;
+    } else {
+        return false;
+    }
+},
+buildSuggestions = function (dpla) {
+    var items = dpla.docs,
+        current = {},
+        suggestions = [];
+
+    $.each(items, function (index, item){
+        var res = item.sourceResource;
+
+        current.title = $.isArray(res.title) ? res.title[0] : res.title;
+        current.title = trunc(current.title);
+        current.uri = item.isShownAt;
+        current.isImage = isItAnImage(res.type);
+
+        suggestions.push(current);
+        current = {};
+    });
+
+    return suggestions;
+},
+// send XHR to DPLA, pass results to callback
+getDPLAresults = function (wp, cb) {
+    var url = buildURI(query);
+
+    $.ajax({
+        url: url,
+        dataType: 'json'
+    })
+    .done(function(data) {
+        var suggestions = buildSuggestions(data);
+
+        // if we didn't get anything, try a fallback
+        if (suggestions.length === 0) {
+            // first look in redirects
+            if (wp.redirects.length !== 0) {
+                query = wp.redirects.pop();
+                // out of redirects? look in categories
+            } else if (wp.categories.length !== 0) {
+                query = wp.categories.pop();
+            } else {
+                // @todo handle this situation
+                console.log('Not a single DPLA search result…damn.');
+            }
+
+            // will use the new query
+            getDPLAresults(wp, cb);
+            return;
+        }
+
+        cb(suggestions);
+    })
+    .fail(function(data, status, xhr) {
+        console.log('XHR error. Status:', status, 'XHR:', xhr);
+    });
+};
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    // contentscript sends wp object with info about article
+    var wp = request,
+        id = sender.tab.id;
+
+    query = wp.title;
+
+    console.log('Message from a content script at', sender.tab.url);
+
+    getDPLAresults(wp, function (suggestions) {
+        // a callback 3rd param to addListener never seems to work
+        // but using this manual callback method does
+        chrome.tabs.sendMessage(id, suggestions);
+    });
+});
---
+++ app/scripts/contentscript.js
@@ -0,0 +1,128 @@
+/* global jQuery */
+/* jQuery is available since it's loaded as another content script
+ * using v. 1.8.3 since that's what Wikipedia uses, helps with consistency
+ * @todo revisit jQuery dependency, probably not necessary
+ *
+ * contentscript.js:
+ * parse the article's HTML into wp object
+ * send {wp} to background.js
+ * on response from background.js, insert results into DOM
+ */
+
+'use strict';
+
+var $ = jQuery,
+wp = {
+    title: $('#firstHeading').text(),
+    redirects: [],
+    categories: [],
+    // find any '"Foo" redirects here.' alternate titles
+    getRedirects: function () {
+        $('.hatnote').each(function (index, el){
+            var test = $(el).text().match('"(.*)" redirects here.');
+
+            if (test) {
+                wp.redirects.push(test[1]);
+            }
+        });
+    },
+    // find the categories on the page
+    getCategories: function () {
+        $('#mw-normal-catlinks li').each(function (index, el){
+            // this == current DOM el, not wp
+            wp.categories.push($(el).text());
+        });
+    }
+},
+// background.js will populate this from the DPLA API
+suggestions = [],
+// on off-chance title contains unescaped HTML,
+// replace any angle brackets < > with HTML entities
+rmAngles = function (str) {
+    return str.replace('<','&lt;').replace('>','&gt;');
+},
+// put constructed HTML into DOM
+addToDOM = function (html) {
+    // #mw-content-text is main body of article
+    $('#mw-content-text').prepend(html);
+    // @todo is this the smoothest way to do this?
+    $('#loaddpla').hide('slow');
+    $('#wikipedpla').show('slow');
+},
+// add HTML to page based on info in suggestions array
+displaySuggestions = function () {
+    // @todo is there a better way to construct this HTML?
+    var html = '<div id="wikipedpla" class="hatnote"><a href="http://dp.la">DPLA</a> ',
+        len = suggestions.length;
+
+    // if we don't have suggestions yet,
+    // wait for background.js to send them to us
+    if (len === 0) {
+        console.log('No suggestions available yet…');
+        setTimeout(displaySuggestions, 300);
+        return;
+    }
+
+    // handling plurals
+    if (len === 1) {
+        html += 'item:';
+    } else {
+        html += 'items:';
+    }
+
+    $.each(suggestions, function (index, item) {
+        var last = (index + 1 === len);
+
+        // len !== 1 prevents "&" added to a list of 1
+        if (last && len !== 1) {
+            html += ' & ';
+        }
+
+        html += ' <a href="' + rmAngles(item.uri) + '"';
+
+        if (item.isImage) {
+            html += ' class="dp-img"';
+        }
+
+        html += '>' + rmAngles(item.title);
+
+        if (!last) {
+            html += '</a>,';
+        } else {
+            html += '</a>.';
+        }
+    });
+
+    html += '</div>';
+
+    addToDOM(html);
+},
+// add #loaddpla link & ask background.js for data
+init = function() {
+    // only execute on the Main (Articles) namespace
+    // the first tab, text "Articles", has an id
+    // of form "cs-nstab-$NAMESPACE"
+    var tab = $('li[id^="ca-nstab-"]');
+
+    if (tab.attr('id').substr(-4) === 'main' &&
+        tab.hasClass('selected') &&
+        $('#ca-view').hasClass('selected') &&
+        tab.text() !== 'Main Page') {
+        // collect page information
+        wp.getRedirects();
+        wp.getCategories();
+
+        // add the #loaddpla link
+        $('#mw-content-text').prepend('<div class="hatnote"><a id="loaddpla">Search the <abbr title="Digital Public Library of America">DPLA</abbr>…</a></div>').find('#loaddpla').show('slow');
+        $('#loaddpla').on('click', displaySuggestions);
+
+        chrome.runtime.sendMessage(wp);
+
+        chrome.runtime.onMessage.addListener(function (req) {
+            suggestions = req;
+            console.log('Suggestions:', suggestions);
+        });
+    }
+};
+
+init();
