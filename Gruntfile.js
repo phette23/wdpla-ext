dee939fe2ab0a9671d968d3dfa72206cca41b421
phette23 <phette23@gmail.com>
2014-08-09 11:15:26 -0700

    get `grunt build` working, slight manifest tweak

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,7 +137,7 @@ module.exports = function (grunt) {
         chromeManifest: {
             dist: {
                 options: {
-                    buildnumber: true,
+                    buildnumber: false,
                     background: {
                         target:'scripts/background.js'
                     }
