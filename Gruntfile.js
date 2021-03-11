16070bb83ad67d8e999485f813e434bff7297f0f
Eric Phetteplace <phette23@gmail.com>
2021-03-10 20:48:00 -0800

    update npm deps
    rm unused one & comment from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,16 +30,9 @@ module.exports = function (grunt) {
             },
             all: [
                 'Gruntfile.js',
-                '<%= yeoman.app %>/scripts/{,*/}*.js'
+                '<%= yeoman.app %>/scripts/*.js'
             ]
         },
-        // eventually use for options.css + bootstrap.css
-        // concat: {
-        //     dist: {
-        //         src: [],
-        //         dest: ''
-        //     }
-        // },
         uglify: {
             dist: {
                 files: {
